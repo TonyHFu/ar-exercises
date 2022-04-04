4d38cf9760f954823a42b7f7abaf2f41af3a0fdf
tofutigerz <hfu3@ualberta.ca>
2022-04-04 11:54:57 -0600

    finishes exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tony", last_name: "Boss", hourly_rate: 6000000)
+@store1.employees.create(first_name: "Bob", last_name: "the Builder III", hourly_rate: 0)
+
+
+@store2.employees.create(first_name: "Thomas", last_name: "the Train Engine", hourly_rate: 999)
+@store2.employees.create(first_name: "Arthur", last_name: "Unknown", hourly_rate: 5)
